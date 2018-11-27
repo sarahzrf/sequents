46013d6418a37703102231254b8fad397422007f
benzrf <benzrf@benzrf.com>
2018-11-27 18:57:23 -0500

    implemented contraction & marking complete proofs. also a bunch of refactoring---but is it really an improvement? look into using optics next.

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,8 @@ module Main where
 import Prelude
 import Data.Array hiding (init)
 import Data.Maybe
+import Data.Monoid
+import Data.Tuple
 
 import Effect (Effect)
 import Spork.Html (Html)
@@ -17,23 +19,50 @@ main = void $ PureApp.makeWithSelector app "body"
 -- MODEL
 
 data Prop = Atom String | Conj Prop Prop | Disj Prop Prop | Neg Prop
-data Sequent = Entails (Array Prop) (Array Prop)
+type Props = Array Prop
+data Sequent = Entails Props Props
 data Proof = Assertion Sequent | Justification (Array Proof) Sequent
+type Model = Proof
 
 derive instance eqProp :: Eq Prop
 
 infix 4 Entails as |-
 
-type Model = Proof
-
 init :: Model
--- init = Assertion $ [Atom "P" `Disj` Atom "P", Atom "Q"] |- [Atom "P"]
--- init = Assertion $ [Atom "P" `Conj` Atom "Q"] |- [Atom "P" `Disj` Atom "Q"]
-init = Assertion $ [Atom "P"] |- [Neg $ Neg $ Atom "P"]
+init = Assertion $
+  [Atom "P" `Conj` (Neg (Atom "P") `Disj` Neg (Neg (Atom "Q")))] |- [Atom "Q"]
 
 endSeq :: Proof -> Sequent
 endSeq (Assertion end) = end
 endSeq (Justification _ end) = end
+
+complete :: Proof -> Boolean
+complete (Assertion _) = false
+complete (Justification subprfs _) = all complete subprfs
+
+data Side = LHS | RHS
+data SeqFocused foc = SF {
+  side :: Side,
+  before :: Props, foc :: foc, after :: Props,
+  oside :: Props}
+
+derive instance functorSequentFocused :: Functor SeqFocused
+
+reassemble :: SeqFocused Props -> Sequent
+reassemble (SF {side, before, foc, after, oside}) = case side of
+  LHS -> before <> foc <> after |- oside
+  RHS -> oside |- before <> foc <> after
+
+-- TODO inefficient!!!
+focuses :: Sequent -> Tuple (Array (SeqFocused Prop)) (Array (SeqFocused Prop))
+focuses (Entails ants sucs) = Tuple (half LHS sucs ants) (half RHS ants sucs)
+  where half side oside =
+          let go before rest = case uncons rest of
+                Nothing -> []
+                Just {head, tail} ->
+                  SF {side, before, foc: head, after: tail, oside} :
+                  go (snoc before head) tail
+          in go []
 
 
 -- UPDATE
@@ -55,19 +84,15 @@ update (Justification subprfs end) (ChildAction ix act) =
 
 -- VIEW
 
-renderDerivation :: Model -> Html Action
-renderDerivation prf = H.div [H.classes ["derivation"]] $ case prf of
-  Assertion end -> [map NAction (renderSequent end)]
-  Justification subprfs end -> [
-    let rsp ix subprf = map (ChildAction ix) (renderDerivation subprf)
-    in H.div [] (mapWithIndex rsp subprfs),
-    H.hr [], map NAction (renderSequent end)]
-
-mapSlices :: forall a b. (Array a -> a -> Array a -> b) -> Array a -> Array b
-mapSlices f = go []
-  where go before rest = case uncons rest of
-          Nothing -> []
-          Just {head, tail} -> f before head tail : go (snoc before head) tail
+renderDerivation :: Proof -> Html Action
+renderDerivation prf =
+  H.div [H.classes ["derivation", guard (complete prf) "complete"]]
+  case prf of
+    Assertion end -> [map NAction (renderSequent end)]
+    Justification subprfs end -> [
+      let rsp ix subprf = map (ChildAction ix) (renderDerivation subprf)
+      in H.div [] (mapWithIndex rsp subprfs),
+      H.hr [], map NAction (renderSequent end)]
 
 -- TODO inefficient!!!
 intersperse :: forall a. a -> Array a -> Array a
@@ -80,45 +105,35 @@ clickable :: forall act. act -> Html act -> Html act
 clickable act body =
   H.span [H.classes ["clickable"], H.onClick (const (Just act))] [body]
 
+rclickable :: forall act. act -> Html act -> Html act
+rclickable act body = H.span [H.on "contextmenu" (const (Just act))] [body]
+
 renderSequent :: Sequent -> Html NodeAction
-renderSequent (Entails ants sucs) = H.span [] $ concat [
-    side (renderAnt sucs) ants,
-    [clickable vdashAct (H.text " ⊢ ")],
-    side (renderSuc ants) sucs]
-  where side f ps = intersperse (H.text ", ") (mapSlices f ps)
-        vdashAct = if any (_ `elem` ants) sucs
-                   then Obligations [] else ClearAbove
+renderSequent seq@(Entails ants sucs) =
+    H.span [] (half focsL <> [vdashActs (H.text " ⊢ ")] <> half focsR)
+  where Tuple focsL focsR = focuses seq
+        half = intersperse (H.text ", ") <<<
+          map (map (Obligations <<< map reassemble) <<< renderProp)
+        vdashActs = rclickable ClearAbove <<<
+          if any (_ `elem` ants) sucs
+            then clickable (Obligations []) else identity
 
-renderAnt :: Array Prop -> Array Prop -> Prop -> Array Prop -> Html NodeAction
-renderAnt sucs before p after = case p of
-  Atom a -> H.span [] [H.text a]
-  Conj l r -> let obsl = [before <> [l] <> after |- sucs]
-                  obsr = [before <> [r] <> after |- sucs]
-              in H.span [] [
-                clickable (Obligations obsl) (renderProp l), H.text " ∧ ",
-                clickable (Obligations obsr) (renderProp r)]
-  Disj l r -> let obs = [before <> [l] <> after |- sucs,
-                         before <> [r] <> after |- sucs]
-              in clickable (Obligations obs) (renderProp p)
-  Neg b -> let obs = [before <> after |- sucs `snoc` b]
-           in clickable (Obligations obs) (renderProp p)
-
-renderSuc :: Array Prop -> Array Prop -> Prop -> Array Prop -> Html NodeAction
-renderSuc ants before p after = case p of
-  Atom a -> H.span [] [H.text a]
-  Conj l r -> let obs = [ants |- before <> [l] <> after,
-                         ants |- before <> [r] <> after]
-              in clickable (Obligations obs) (renderProp p)
-  Disj l r -> let obsl = [ants |- before <> [l] <> after]
-                  obsr = [ants |- before <> [r] <> after]
-              in H.span [] [
-                clickable (Obligations obsl) (renderProp l), H.text " ∨ ",
-                clickable (Obligations obsr) (renderProp r)]
-  Neg b -> let obs = [ants `snoc` b |- before <> after]
-           in clickable (Obligations obs) (renderProp p)
-
-renderProp :: forall act. Prop -> Html act
-renderProp = H.text <<< ppProp
+-- interacting with this node can produce an array of obligations, with each
+-- obligation in focused form
+renderProp :: SeqFocused Prop -> Html (Array (SeqFocused Props))
+renderProp sf@(SF {side, foc}) =
+  rclickable [[foc, foc] <$ sf] case side, foc of
+    _, Atom _ -> H.span [] [ppPropH foc]
+    LHS, Conj l r -> H.span [] [
+      clickable [[l] <$ sf] (ppPropH l), H.text " ∧ ",
+      clickable [[r] <$ sf] (ppPropH r)]
+    RHS, Disj l r -> H.span [] [
+      clickable [[l] <$ sf] (ppPropH l), H.text " ∨ ",
+      clickable [[r] <$ sf] (ppPropH r)]
+    LHS, Disj l r -> clickable [[l] <$ sf, [r] <$ sf] (ppPropH foc)
+    RHS, Conj l r -> clickable [[l] <$ sf, [r] <$ sf] (ppPropH foc)
+    _, Neg b -> let SF c = [] <$ sf
+                in clickable [SF c{oside = c.oside `snoc` b}] (ppPropH foc)
 
 -- TODO precedence
 ppProp :: Prop -> String
@@ -127,6 +142,9 @@ ppProp p = case p of
   Conj l r -> ppProp l <> " ∧ " <> ppProp r
   Disj l r -> ppProp l <> " ∨ " <> ppProp r
   Neg b -> "¬" <> ppProp b
+
+ppPropH :: forall act. Prop -> Html act
+ppPropH = H.text <<< ppProp
 
 
 render :: Model -> Html Action
